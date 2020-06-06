f9df24c15c28e7967801d2b7461320fada48e4e6
Daniel C <danclayson@gmail.com>
2020-06-05 22:51:47 -0400

    Switch input to password

--- src/Pages/Settings.tsx
+++ src/Pages/Settings.tsx
@@ -10,7 +10,7 @@ export const SettingsPage = observer(()=>{
             <h2>Settings</h2>
             <div style={{display: "flex", flexDirection: "column"}}>
                 <label htmlFor={"apiToken"}><h4>Toggl API Token</h4></label>
-                <input id={'apiToken'} placeholder={"Put your token here..."} type={'text'} value={appState.settings.apiToken} onChange={(e)=>{appState.settings.setApiToken(e.currentTarget.value)}}/>
+                <input id={'apiToken'} placeholder={"Put your token here..."} type={'password'} value={appState.settings.apiToken} onChange={(e)=>{appState.settings.setApiToken(e.currentTarget.value)}}/>
                 <small>Find your API token at the bottom of <a href={'https://toggl.com/app/profile'} rel="noopener noreferrer" target={"_blank"}>toggl.com/app/profile</a> </small>
             </div>
             <div style={{marginTop: "50px"}}>
