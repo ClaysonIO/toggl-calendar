4542d0663062a20fed7fa7d6c522b1f5f22bf9be
Daniel C <danclayson@gmail.com>
2020-06-06 08:36:36 -0400

    UI changes

--- src/Pages/Main.tsx
+++ src/Pages/Main.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import {Layout} from "../Components/Layout";
+import { Link } from "react-router-dom";
 
 export const MainPage = ()=>{
     return (
         <Layout>
             <h2>What is Toggl Calendar View?</h2>
-            <p>An easy way to get daily information out of Toggl, and into another tool, by streamlining the copy-past process</p>
+            <p>An easy way to get daily information out of Toggl, and into another tool, by streamlining the copy-paste process</p>
+            <p>To get started, add your API Token in <Link to={"/settings"}>Settings</Link></p>
         </Layout>
     )
 }
--- src/Pages/Settings.tsx
+++ src/Pages/Settings.tsx
@@ -8,20 +8,27 @@ export const SettingsPage = observer(()=>{
     return (
         <Layout>
             <h2>Settings</h2>
-            <div style={{display: "flex", flexDirection: "column"}}>
-                <label htmlFor={"apiToken"}><h4>Toggl API Token</h4></label>
+            <section style={{display: "flex", flexDirection: "column"}}>
+                <label htmlFor={"apiToken"}><h3>Toggl API Token</h3></label>
                 <input id={'apiToken'} placeholder={"Put your token here..."} type={'password'} value={appState.settings.apiToken} onChange={(e)=>{appState.settings.setApiToken(e.currentTarget.value)}}/>
                 <small>Find your API token at the bottom of <a href={'https://toggl.com/app/profile'} rel="noopener noreferrer" target={"_blank"}>toggl.com/app/profile</a> </small>
-            </div>
-            <div style={{marginTop: "50px"}}>
-                <h3>Toggl Workspaces</h3>
+            </section>
+            <section style={{marginTop: "50px"}}>
+                <div style={{display: 'flex'}}>
+                    <h3 style={{flex: 1}}>Toggl Workspaces</h3>
+                    <button onClick={appState.getWorkSpaces} disabled={!appState.settings.apiToken}>Fetch Workspaces</button>
+                </div>
                 <hr/>
 
-                <ul>
-                    {appState.workSpaces.map((val, index)=>(<li key={index}>{val.name}</li>))}
-                </ul>
-                <button onClick={appState.getWorkSpaces} disabled={!appState.settings.apiToken}>Fetch Workspaces</button>
-            </div>
+                {appState.workSpaces.length ?
+                    <ul>
+                        {appState.workSpaces.map((val, index) => (<li key={index}>{val.name}</li>))}
+                    </ul>
+                    :
+                    <h4 style={{textAlign: 'center'}}>- No Workspaces Retrieved - </h4>
+                }
+
+            </section>
         </Layout>
     )
 })
