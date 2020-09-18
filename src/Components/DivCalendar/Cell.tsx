b4951169b804c2b412a3eb6035e5326fdc28290c
Daniel C <danclayson@gmail.com>
2020-09-18 10:53:05 -0400

    Don't increase size of cells. Put copied text into tippy. Create title for text

--- src/Components/DivCalendar/Cell.tsx
+++ src/Components/DivCalendar/Cell.tsx
@@ -26,20 +26,34 @@ export const Cell = ({day, displayType, expanded}: { day?: Day, displayType: str
     }
 
     let value: string | ReactElement | ReactElement[] = "";
+    let valueString: string = '';
 
     if(day){
         switch(displayType){
-            case "description": value = day.tasks.map((v, index)=>(<div key={index}>{v}</div>)); break;
-            case "time": value = day.hours; break;
-            case "roundedTime": value = day.roundedHours; break;
-            case "tasksAndTime": value = day.tasksAndRoundedTime.map((v, index)=>(<div key={index}>{v}</div>)); break;
+            case "description":
+                value = day.tasks.map((v, index)=>(<div key={index}>{v}</div>));
+                valueString = day.tasks.join('\n');
+                break;
+            case "time":
+                value = day.hours;
+                valueString = day.hours;
+                break;
+            case "roundedTime":
+                value = day.roundedHours;
+                valueString = day.roundedHours;
+                break;
+            case "tasksAndTime":
+                value = day.tasksAndRoundedTime.map((v, index)=>(<div key={index}>{v}</div>));
+                valueString = day.tasksAndRoundedTime.join('\n');
+                break;
         }
     }
 
     return (
-        <div className={`dateCol ${expanded ? 'expanded' : ''}`}>
+        // <div className={`dateCol ${expanded ? 'expanded' : ''}`} title={valueString}>
+         <div className={`dateCol`} title={valueString}>
             <Tippy
-                content={"Copied"}
+                content={`Copied: \n ${valueString}`}
                 visible={visible}
                 onClickOutside={hide}
             >
